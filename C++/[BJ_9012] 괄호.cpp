887b442f30cdfa863cfa00da0ae932099a041831
Lukas <lukasjung114@gmail.com>
2025-06-24 17:50:13 +0900

    C++ 파일 수정: [BJ_9012] 괄호.cpp에서 벡터 포함 및 코드 구조 개선

--- C++/[BJ_9012] 괄호.cpp
+++ C++/[BJ_9012] 괄호.cpp
@@ -6,6 +6,8 @@ Link            : https://www.acmicpc.net/problem/9012
 */
 
 #include <iostream>
+#include <vector>
+
 
 using namespace std;
 
@@ -13,7 +15,44 @@ int main(void){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    
+    int T;
+    cin >> T;
 
+    int len = 0;
+    int cnt = 0;
+    vector<char> arr;
+    vector<char> :: iterator it;
+    string s;
+    while(T){
+        cin >> s;
+        len = s.length();
+        // 길이가 홀수인 경우 조기종료
+        if ((len%2 != 0) || (s[0] == ')') || (s[len-1] == '(')){
+            cout << "No\n";
+            continue;
+        }
+        
+        // vector에 대입
+        for(int i = 0; i < len; i++){
+            arr.push_back(s[i]);
+        }
+        it = arr.begin();
+        for(int i = 1; i < len; i++){
+            if ((arr[i-1] == '(') && (arr[i] == ')')){
+
+            }
+        }
+        for (;it!=arr.end(); it++){
+            
+        }
+
+
+
+
+
+
+
+        T--;
+    }
 
 }
