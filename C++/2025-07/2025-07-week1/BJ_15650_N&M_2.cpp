003a94d3b50a8051a86cd9b7d1a2401a3bf81f21
Lukas <lukasjung114@gmail.com>
2025-07-03 13:29:42 +0900

    C++ 파일 추가: [BJ_15650] N과 M (2) 문제 해결 코드 및 테스트 케이스 추가

--- C++/2025-07/2025-07-week1/BJ_15650_N&M_2.cpp
+++ C++/2025-07/2025-07-week1/BJ_15650_N&M_2.cpp
@@ -6,13 +6,40 @@ Link            : https://www.acmicpc.net/problem/15650
 */
 
 #include <iostream>
+#include <set>
+#define Max 9
 
 using namespace std;
+
+int N, M;
+int NumArr[Max];
+bool visited[Max];
+
+
+void DFS(int initiateNum, int depth){
+   if (depth == M){
+    for(int i = 0; i < M; i++){
+        cout << NumArr[i] << " ";
+    }
+    cout << "\n";
+    return;
+   }
+
+   for(int i = initiateNum; i <= N; i++){
+    if (!visited[i]){
+        visited[i] = true;
+        NumArr[depth] = i;
+        DFS(i, depth + 1);
+        visited[i] = false;
+    }
+   }
+}
+
 
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
-
-
+    cin >> N >> M;
+    DFS(1, 0);
 }
