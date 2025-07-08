403815634a599981884aa44c435d8ad1c7e2898a
Lukas <lukasjung114@gmail.com>
2025-07-08 14:09:59 +0900

    C++ 파일 수정: 알고리즘 수업 - BJ_24479 깊이 우선 탐색 1 구현 및 로직 개선. 풀이 완료

--- C++/2025-07/2025-07-week2/BJ_24479_DFS_1.cpp
+++ C++/2025-07/2025-07-week2/BJ_24479_DFS_1.cpp
@@ -7,14 +7,27 @@ Link            : https://www.acmicpc.net/problem/24479
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int N, M, R;
+int visitCNT = 0;
 
+//? V가 필요한가?
 // V : 정점 집합, E : 간선 집합, R : 시작 노드
-void DFS(){
-
+void DFS(vector<vector<int>> &E, int R, vector<bool> &visited, vector<int> &cnt){
+    //! 1. 방문한 노드 체크
+    visited[R] = true;
+    ++visitCNT;
+    cnt[R] = visitCNT;
+    //! 2. 방문한 노드에 연결된 노드들을 확인
+    for(auto v : E[R]){
+        //! 3. 연결된 노드들 중 방문하지 않은 노드 방문 (오름차순으로 정렬되어 있으므로 순서대로 방문)
+        if(!visited[v]){
+            DFS(E, v, visited, cnt);
+        }
+    }
 
 }
 
@@ -25,22 +38,35 @@ int main(void){
 
     cin >> N >> M >> R;
 
-    // V (Node 집합) 입력 및 선언
-    vector<int> V(N+1);
-    for(int i = 1; i <= N; i++){
-        V[i] = i;
-    }
+    // //* V (Node 집합) 입력 및 선언
+    // vector<int> V(N+1);
+    // for(int i = 1; i <= N; i++){
+    //     V[i] = i;
+    // }
 
-    // E (Edge 집합) 입력 및 선언
-    vector <vector<int>> E(N+1, vector<int>(N+1,0));
+    //* Edge 집합 선언
+    vector <vector<int>> E(N+1, vector<int>());
     int tmpA, tmpB;
     for (int i = 0; i < M; i++){
         cin >> tmpA >> tmpB;
-        E[tmpA][tmpB] = 1;      // 엣지가 있는 곳은 1로 표현
-        E[tmpB][tmpA] = 1;
+        E[tmpA].push_back(tmpB);
+        E[tmpB].push_back(tmpA);
     }
 
+    //* 노드 별로 연결된 노드 오름차순 정렬
+    for (int i = 1; i <= N; i++){
+        sort(E[i].begin(), E[i].end());
+    }
 
+    vector<bool> visited(N+1, false);
+    vector<int> cnt(N+1,0);
+
+    DFS(E, R, visited, cnt);
 
     
+    //* 출력
+    for(int i = 1; i <= N; i++){
+        cout << cnt[i] << "\n";
+    }
+
 }
