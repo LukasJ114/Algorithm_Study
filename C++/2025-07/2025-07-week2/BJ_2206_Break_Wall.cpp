37b1fc5bdb35c118a7e6fbd422a8a5b4d1746b01
Lukas <lukasjung114@gmail.com>
2025-07-12 20:00:38 +0900

    C++ 파일 수정: 알고리즘 수업 - BJ_2206 벽 부수고 이동하기 코드 개선 및 구조화 [풀이완료]

--- C++/2025-07/2025-07-week2/BJ_2206_Break_Wall.cpp
+++ C++/2025-07/2025-07-week2/BJ_2206_Break_Wall.cpp
@@ -13,15 +13,26 @@ using namespace std;
 int N, M;
 int **Map;
 
+struct Wall{
+    int row;
+    int col;
+    int bomb;
+    Wall(int r, int c, int b){
+        row = r;
+        col = c;
+        bomb = b;
+    }
+};
+
 
 void Input(){
     cin >> N >> M;
 
     //* Map 선언
-    Map = new int* [N+1];
-    for(int i = 0; i < N+1; i++){
-        Map[i] = new int [M+1];
-        for(int j = 0; j < M+1; j++){
+    Map = new int* [N+2];
+    for(int i = 0; i < N+2; i++){
+        Map[i] = new int [M+2];
+        for(int j = 0; j < M+2; j++){
             Map[i][j] = 0;
         }
     }
@@ -31,38 +42,48 @@ void Input(){
         string input;
         cin >> input;
         for (int j = 1; j <= M; j++){
-            Map[i][j] = input[j-1];
+            Map[i][j] = input[j-1] - '0';
         }
     }
 
 }
-
-void BFS(int startRow, int startCol, int wall){
+// wall : 벽을 부쉈는지 여부
+void BFS(int startRow, int startCol, int brokeWall){
+    // //! Debugging Code
+    // cout << "BFS 시작\n";
 
     //* visited 동적할당 및 초기화
     int ***visited;
-    visited = new int**[N+1];
-    for(int i = 0; i < N+1; i++){
-        visited[i] = new int*[M+1];
-        for(int j = 0; j < M+1; j++){
+    visited = new int**[N+2];
+    for(int i = 0; i < N+2; i++){
+        visited[i] = new int*[M+2];
+        for(int j = 0; j < M+2; j++){
             visited[i][j] = new int[2];
             visited[i][j][0] = 0;
             visited[i][j][1] = 0;
         }
     }
     //* visited row = 0, col = 0 접근하지 못하도록 visited = 1로 세팅
-    for(int i = 0; i < N+1; i++){
+    for(int i = 0; i < N+2; i++){   // 0번쨰, M+1 번째 열을 1로 세팅
         visited[i][0][0] = 1;
         visited[i][0][1] = 1;
+        
+        visited[i][M+1][0] = 1;
+        visited[i][M+1][1] = 1;
     }
-    for(int i = 0; i < M+1; i++){
+    for(int i = 0; i < M+2; i++){   // 0번째, N+1 번째 행을 1로 세팅
         visited[0][i][0] = 1;
         visited[0][i][1] = 1;
+
+        visited[N+1][i][0] = 1;
+        visited[N+1][i][1] = 1;
     }
     //* 도착지 기본값 -1로 세팅
-    visited[N][M][0] = -1;
-    visited[N][M][1] = -1;
+    // visited[N][M][0] = -1;
+    // visited[N][M][1] = -1;
 
+    // //! Debugging Code
+    // cout << "visited 할당 및 선언 완료\n";
 
 
 
@@ -70,21 +91,95 @@ void BFS(int startRow, int startCol, int wall){
     int dr[4] = {1, -1, 0, 0};
     int dc[4] = {0, 0, -1, 1};
 
-    queue<tuple<int,int,int>> q;
-    // q.push({1,2,3});
-    // get<1>(q.front());
+    queue<Wall> q;
     
     //* 최초 시작 좌표 대입
-    q.push({startRow, startCol, wall});
+    // q.push({startRow, startCol, wall});     //? Struct인데 이렇게 삽입 가능한가? 
+    q.push(Wall(startRow, startCol, brokeWall));    //! 역시 안된다.
+    
+    //* 최초 시작 좌표 방문 체크
+    visited[startRow][startCol][0] = 1;
+    
+    int res = 0;
+    //* 탐색 시작. 
+    while(!q.empty()){
+        int nowRow = q.front().row;
+        int nowCol = q.front().col;
+        int nowBomb = q.front().bomb;
+        q.pop();
+
+        
+        if (nowRow == N && nowCol == M){
+            res = visited[nowRow][nowCol][nowBomb];
+            // if (res == 0) cout << "-1";
+            // else cout << res;
+            // return;
+            break;
+        }
+
+
+        for(int i = 0; i < 4; i++){
+            int nextRow = nowRow + dr[i];
+            int nextCol = nowCol + dc[i];
+            // 내가 방문한 곳이 벽인가 아닌가. (1이면 벽. 0이면 벽 없음)
+            int isWall = Map[nextRow][nextCol];
+
+            
+            //* 잘못된 인덱스 접근 차단
+            if(nextRow < 1 || nextCol < 1 || nextRow > N || nextCol > M) continue;
+
+            /*
+            * 고려해야 할 경우의 수 *
+            TODO 1. 탐색한 곳이 벽 && 폭탄을 가지고 있다 ==> 폭탄을 터트리고 진입
+            TODO 2. 탐색한 곳이 벽 && 폭탄이 없음 ==>  진입불가
+            TODO 3. 탐색한 곳이 벽이 아님. ==> 아무 문제 없음. 진입
+            */
+
+            /*
+            //* TODO 1 => 탐색하는 곳이 벽이면서 내가 폭탄을 가지고 있다.
+            if(visited[nextRow][nextCol][nowBomb] == 0 && isWall && nowBomb == 0){
+                visited[nextRow][nextCol][1] = visited[nowRow][nowCol][nowBomb]+ 1;
+                visited[nextRow][nextCol][0] = 1;   //예외처리
+                q.push(Wall(nextRow, nextCol, 1));
+                continue;
+            }
+            //* TODO 2 => 진입못함. 
+            else if(isWall && nowBomb == 1) continue;
+            
+            //* TODO 3 => 방문한적 없고, 벽 없음. =>> 그냥 집입하면 된다.
+            else if(visited[nextRow][nextCol][nowBomb] == 0 && !isWall){
+                visited[nextRow][nextCol][nowBomb] = visited[nowRow][nowCol][nowBomb]+ 1;
+                q.push(Wall(nextRow, nextCol, nowBomb));
+                
+            }
+            */
+
+            if(!isWall){    // wall이 없을 때
+                // 다음 이동하는 곳이 0일때
+                if(visited[nextRow][nextCol][nowBomb] == 0){
+                    visited[nextRow][nextCol][nowBomb] = visited[nowRow][nowCol][nowBomb]+ 1;
+                    q.push(Wall(nextRow, nextCol, nowBomb));
+                }
+            }
+            else{   // wall이 있을때
+                if(visited[nextRow][nextCol][1] == 0 && nowBomb == 0){
+                    visited[nextRow][nextCol][1] = visited[nowRow][nowCol][nowBomb] + 1;
+                    // visited[nextRow][nextCol][0] = 1;   //예외처리
+                    q.push(Wall(nextRow, nextCol, 1));
+                }
+            }
+        }
+    }
 
 
 
-
+    if (res == 0) cout << "-1";
+    else cout << res;
 
 
     //* visited 동적할당 해제
-    for(int i = 0; i < N+1; i++){
-        for(int j = 0; j < M+1; j++){
+    for(int i = 0; i < N+2; i++){
+        for(int j = 0; j < M+2; j++){
             delete[] visited[i][j];
         }
         delete[] visited[i];
@@ -100,13 +195,17 @@ int main(void){
 
     Input();
 
-
-
+    // //! Debugging Code
+    // cout << "input 완료" << endl;
+    
+    BFS(1,1,0);
 
 
     //* Map 동적할당 해제
-    for(int i = 0; i < N+1; i++){
+    for(int i = 0; i < N+2; i++){
         delete[] Map[i];
     }
     delete[] Map;
 }
+
+
