9622d900e023a7e31164b8cf234d8eac66d594a6
Lukas <lukasjung114@gmail.com>
2025-07-29 17:42:50 +0900

    C++ 파일 수정: BOJ_1463_Make1 문제 해결을 위한 코드 구현 및 테스트 케이스 추가 퇴사2 - 반례 발견

--- C++/2025-07/2025-07-week5/BOJ_1463_Make1.cpp
+++ C++/2025-07/2025-07-week5/BOJ_1463_Make1.cpp
@@ -6,10 +6,36 @@ Link            : https://www.acmicpc.net/problem/15486
 */
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main(void){
+int N;
 
+int main(void){
+    cin >> N;
+
+    vector<int> table(N+1, N);
+    table[1] = 0;
+    table[2] = 1;
+    table[3] = 1;
+
+    for(int i = 4; i <= N; i++){
+        int tmp = N;
+        if((i % 3 == 0) || (i % 2 == 0)){
+            if(i % 3 == 0){
+                table[i] = (table[i/3] + 1) < tmp ? (table[i/3] + 1) : tmp;
+            }
+            if(i % 2 == 0){
+                table[i] = (table[i/2] + 1) < tmp ? (table[i/2] + 1) : tmp;
+            }
+        }
+        
+        if(table[i] > (table[i-1] + 1)) table[i] = table[i-1] + 1;
+        // table[i] = (table[i-1] + 1) < tmp ? (table[i-1] + 1) : tmp;
     
+    }
+    cout << table[N];
+
+
 }
